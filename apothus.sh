8c4b65622bdec6ea11599302e34cc59015b63827
YunIron <69750956+YunIron@users.noreply.github.com>
2022-07-24 23:48:15 +0300

    Update apothus.sh

--- apothus.sh
+++ apothus.sh
@@ -1,5 +1,4 @@
-#! /bin/bash/
-
+#!/bin/bash
 
 root() {
     if [[ $EUID -ne 0 ]]; then
@@ -10,36 +9,59 @@ root() {
     fi
 }
 
+main(){
+    checkdata
+    namecard=$(airmon-ng| grep -n "phy"| cut -c25-66)
+    echo "$wlan | $namecard"
+    xterm -hold -title "STORM BREAKER WİFİ SCANNER" -geometry 110x50 -sb -sl 1000 -e "sudo airodump-ng -w input --output-format "csv" $wlan"
+	while IFS= read -r line
+	do
+        echo "$line"
+    done < data-01.csv
+}
+
+
 monitoron() {
         echo "Monitor moda geciliyor..."
         iwconfig $1 > /dev/null 2>&1
-        if [ "$?" = 250 ]; then
+        if [ "$?" = 237 ]; then
             echo "Boyle bir cihaz yok!"
-            exit 1
+            exit
         fi
-        ifconfig $1 down
-        iwconfig $1 mode monitor > /dev/null 2>&1
-        ifconfig $1 up
-        if [[ ! $(iwconfig $1|grep -o "Mode:[A-Za-z]*"|cut -d ":" -f2) == "Monitor" ]]; then
-            echo $(iwconfig $1|grep -o "Mode:[A-Za-z]*"|cut -d ":" -f2)
-            echo "Lütfen $1 adlı arayüzün sürücülerinin tam veya güncel olduğundan emin olun bundan eminseniz ve hala hata alıyorsanız ya sürücülerinizde ya da $1 adlı arayüzünüz monitor modu desteklemiyor olabilir"
-            sleep 2
+        ifconfig "$wlan" down
+        iwconfig "$wlan" mode monitor > /dev/null 2>&1
+        ifconfig "$wlan" up
+        if [[ ! $(iwconfig "$wlan"|grep -o "Mode:[A-Za-z]*"|cut -d ":" -f2) == "Monitor" ]]; then
+            echo "Lütfen $wlan adlı arayüzün sürücülerinin tam veya güncel olduğundan emin olun bundan eminseniz ve hala hata alıyorsanız ya sürücülerinizde ya da $1 adlı arayüzünüz monitor modu desteklemiyor olabilir"
+            sleep 2&
+            wait
             exit
         else
-            echo "$1.......................MONİTOR"
+            clear
+            echo "$wlan.......................MONİTOR"
+            main
         fi
 
 }
 
 monitorcheck() {
-        if [[ $(iwconfig $1|grep -o "Mode:[A-Za-z]*"|cut -d ":" -f2) == 'Monitor' ]]; then
+        if [[ $(iwconfig "$wlan"|grep -o "Mode:[A-Za-z]*"|cut -d ":" -f2) == 'Monitor' ]]; then
             echo "Monitor mod'da [+]"
+            main
         else
             echo "Monitor mod'da degil."
             monitoron
         fi
 }
 
+checkdata() {
+    if [[ -e "$(pwd)/data-01.csv") ]]; then
+        rm -rf $(pwd)/data-01.csv
+    else
+        echo ""
+    fi
+}
 
+wlan=$1
 root
 monitorcheck
